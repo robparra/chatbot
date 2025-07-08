e56a45b4c3a8efcd4df980dfb51d9c9c7e2c29eb
robert <parralrob@gmail.com>
2025-07-08 14:18:39 -0400

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-require('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
